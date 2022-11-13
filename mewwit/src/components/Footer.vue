e804c37a45bebc70cc70d22593b87d670bc02a11
Mattmaxx04 <95719090+Mattmaxx04@users.noreply.github.com>
2022-11-13 19:26:38 -0300

    Rules and footer

---
+++ mewwit/src/components/Footer.vue
@@ -0,0 +1,30 @@
+<script setup></script>
+
+<template>
+    <div class="footer d-flex">
+        <h5>Mewwit</h5>
+        <p>Diseñado por Matías Quiroga <a href="https://www.linkedin.com/in/mat%C3%ADasquiroga/"><i class="bi bi-linkedin"></i></a></p>
+    </div>
+</template>
+
+<style scoped>
+.footer{
+    min-height: 8vh;
+    background: var(--color6);
+    border-top: 0.2rem solid var(--color3) !important;
+    color: var(--color4);
+    padding: 1rem;
+    flex-wrap: wrap;
+    justify-content: space-between;
+    align-items: flex-start;
+}
+.footer > h5{
+    font-size: 1.5rem;
+    font-weight: 800;
+}
+
+.footer > p{
+    font-size: 1.1rem;
+    padding: 0.2rem;
+}
+</style>
--- mewwit/src/views/RulesView.vue
+++ mewwit/src/views/RulesView.vue
@@ -3,9 +3,74 @@
 </script>
 
 <template>
-  
+    <div class="rules container">
+        <h3>Mewwit Content Policy</h3>
+        <p>Mewwit is a vast communitie that is created, run, and populated by you: the Mewwit users.
+        </p>
+        <p>Through these communitie, you can post, comment, vote, discuss, learn, debate, support, and connect with
+            people who share your interest, and we encourage you to find or even create your home on Mewwit.</p>
+        <p>Everyone on Mewwit should have an expectation of privacy and safety, so please respect the privacy and safety
+            of others.
+        </p>
+        <p>Mewwit and its communities are only what we make of them together, and can only exist if we operate by a
+            shared set of rules. We ask that you abide by not just the letter of these rules, but the spirit as well.
+        </p>
+        <h4>Rules</h4>
+        <h6>Rule 1</h6>
+        <p>
+            Remember the human. Mewwit is a place for creating community and belonging, not for attacking marginalized
+            or vulnerable groups of people. Everyone has a right to use Mewwit free of harassment, bullying, and threats
+            of violence. Post that incite violence or that promote hate based on identity or
+            vulnerability will be banned.
+        </p>
+        <h6>Rule 2
+        </h6>
+        <p>Abide by community rules. Post authentic content into post where you have a personal interest, and do
+            not cheat or engage in content manipulation (including spamming, vote manipulation, ban evasion, or
+            subscriber fraud) or otherwise interfere with or disrupt Mewwit communitie.
+
+        </p>
+        <h6>Rule 3</h6>
+        <p>Respect the privacy of others. Instigating harassment, for example by revealing someone’s personal or
+            confidential information, is not allowed. Never post or threaten to post intimate or sexually-explicit media
+            of someone without their consent.</p>
+        <h6>
+            Rule 4
+
+        </h6>
+        <p>Do not post or encourage the posting of sexual or suggestive content involving minors.
+
+        </p>
+        <h6>
+            Rule 5
+
+        </h6>
+        <p>
+            You don’t have to use your real name to use Mewwit, but don’t impersonate an individual or an entity in a
+            misleading or deceptive manner.
+        </p>
+        <h6>
+            Rule 6
+        </h6>
+        <p>
+            Keep it legal, and avoid posting illegal content or soliciting or facilitating illegal or prohibited
+            transactions.
+        </p>
+        <h6>Rules 7</h6>
+        <p>Don’t break the site or do anything that interferes with normal use of Mewwit.
+
+</p>
+    </div>
 </template>
 
 <style scoped>
-
+.rules {
+    border: 2px solid var(--color5) !important;
+    border-radius: 1rem;
+    color: var(--color2);
+    padding: 0.5rem;
+    margin: 1rem;
+    max-width: 90%;
+    min-height: 80vh;
+}
 </style>
