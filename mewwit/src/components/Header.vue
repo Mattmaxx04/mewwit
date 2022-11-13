ee76f266d550f2c12a33aa09e814bcdef703f053
Mattmaxx04 <95719090+Mattmaxx04@users.noreply.github.com>
2022-11-13 19:26:19 -0300

    Routing config

---
+++ mewwit/src/components/Header.vue
@@ -0,0 +1,17 @@
+<script setup>
+
+</script>
+
+<template>
+  <nav class="navbar bg-dark">
+  <div class="container justify-content-center">
+      <img src="@/assets/logomew.png" alt="Bootstrap" width="200" >
+  </div>
+  </nav>
+</template>
+
+<style scoped>
+nav{
+  border-bottom: 0.2rem solid var(--color3) !important;
+}
+</style>
---
+++ mewwit/src/components/Nav.vue
@@ -0,0 +1,48 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+
+</script>
+
+<template>
+    
+    <button class="btn huella" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasExample" aria-controls="offcanvasExample">
+        <img src="@/assets/huella.png" alt="" width="30">
+    </button>
+    <div class="offcanvas offcanvas-start d-flex" tabindex="-1" id="offcanvasExample" aria-labelledby="offcanvasExampleLabel">
+       
+            <RouterLink to="/"><i class="bi bi-house-door"></i></RouterLink>
+            <RouterLink to="/posts"><i class="bi bi-card-list"></i></RouterLink>
+            <RouterLink to="/profile"><i class="bi bi-person-circle"></i></RouterLink>
+            <RouterLink to="/contact"><i class="bi bi-envelope-paper"></i></RouterLink>
+            <RouterLink to="/rules"><i class="bi bi-info-circle"></i></RouterLink>
+        
+    </div>
+
+    
+</template>
+
+<style scoped>
+.huella{
+    background: var(--color3);
+    margin: 0.5rem;
+}
+.offcanvas, .offcanvas-start{
+    margin-top: 4.2rem;
+    margin-left: 0.5rem;
+    width: 20%!important;
+    height: 30%;
+    background: var(--color1);
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    border-radius: 1rem;
+    padding: 2rem;
+    flex-wrap: nowrap;
+    z-index: 9999!important;
+}
+.bi{
+color:var(--color3);
+font-size: 2rem;
+}
+
+</style>
