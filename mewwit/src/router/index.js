ab0704d148d87bdc589cc21c3436c52f71d48320
Mattmaxx04 <95719090+Mattmaxx04@users.noreply.github.com>
2022-11-13 11:51:23 -0300

    Start
    Routing, views, and basic structure

---
+++ mewwit/src/router/index.js
@@ -0,0 +1,55 @@
+import { createRouter, createWebHistory } from "vue-router";
+import HomeView from "../views/HomeView.vue";
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: "/",
+      name: "home",
+      component: HomeView,
+    },
+    {
+      path: "/contact",
+      name: "contact",
+      component: () => import("../views/ContactView.vue"),
+    },
+    {
+      path: "/details",
+      name: "details",
+      component: () => import("../views/DetailsView.vue"),
+    },
+    {
+      path: "/edit",
+      name: "edit",
+      component: () => import("../views/EditView.vue"),
+    },
+    {
+      path: "/favorites",
+      name: "favorites",
+      component: () => import("../views/FavoritesView.vue"),
+    },
+    {
+      path: "/posts",
+      name: "posts",
+      component: () => import("../views/PostsView.vue"),
+    },
+    {
+      path: "/profile",
+      name: "profile",
+      component: () => import("../views/ProfileView.vue"),
+    },
+    {
+      path: "/rules",
+      name: "rules",
+      component: () => import("../views/RulesView.vue"),
+    },
+    {
+      path: "/settings",
+      name: "settings",
+      component: () => import("../views/settingsView.vue"),
+    },
+  ],
+});
+
+export default router;
