c7e3adcfdb91b20672fc2e106e0f609f11cbc69c
Mattmaxx04 <95719090+Mattmaxx04@users.noreply.github.com>
2022-12-14 10:59:17 -0300

    post struture

---
+++ mewwit/src/components/NewPost.vue
@@ -0,0 +1,108 @@
+<script setup>
+import {addPost} from '../firebase/posts.js'
+import { ref } from 'vue'
+import user from '../store/profile.js'
+
+
+const post_id = ref('')
+const post_date = ref('')
+const post_img = ref('')
+const post_title = ref('')
+const post_body = ref('')
+const post_userid = ref('')
+const post_uphoto = ref('')
+const post_username = ref('')
+
+const addNewPost = () => {
+    const newPost = {
+        post_id: crypto.randomUUID(),
+        post_date: new Date(),
+        post_img: post_img.value,
+        post_title: post_title.value,
+        post_body: post_body.value,
+        post_userid: user.value.id,
+        post_uphoto: user.value.photoURL,
+        post_username: user.value.displayName
+    }
+    addPost(newPost)
+    post_body.value=" "
+    post_img.value=" "
+    post_title.value=" "
+}
+</script>
+
+<template>
+<!-- <div class="post__btn" v-if="user">-->
+    <div class="post__btn" >
+        <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal2">
+            Post creation </button>
+
+    </div>
+
+    <div class="modal fade" id="exampleModal2" tabindex="-1" aria-labelledby="exampleModal2Label" aria-hidden="true">
+
+        <div class="modal-dialog modal-dialog-centered" role="document">
+            <div class="modal-content rounded-4 shadow">
+                <div class="modal-header p-5 pb-4 border-bottom-0">
+                    <h3 class="fw-bold mt-2 fs-2 sign">Post creation</h3>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+
+                <div class="modal-body p-5 pt-0">
+                    <form class="">
+                        <div class="form-floating mb-2">
+                            <input v-model="post_title" type="text" class="form-control rounded-3" id="name"
+                                placeholder="name">
+                            <label for="name">title</label>
+                        </div>
+                        <div class="form-floating mb-2">
+                            <input v-model="post_img" type="url" class="form-control rounded-3" id="url"
+                                placeholder="https:\\yourimage.png">
+                            <label for="url">Image url</label>
+                        </div>
+                        <div class="form-floating mb-2">
+                            <input v-model="post_body" type="text" class="form-control rounded-3" id="title"
+                                placeholder="your title">
+                            <label for="title">message</label>
+                        </div>
+                        <button class="w-100 mb-2 btn btn-lg rounded-3 btn-primary" type="submit"
+                            @click.prevent="addNewPost" data-bs-dismiss="modal">Save</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</template>
+
+<style scoped>
+.post__btn {
+    display: flex;
+    justify-content: center;
+    margin: 1rem;
+}
+
+.form__post {
+    height: 8rem !important;
+}
+
+.card__fav {
+    justify-content: space-between;
+    align-items: center;
+}
+
+.post {
+    padding-top: 2rem;
+    gap: 1rem;
+    flex-direction: column;
+    align-items: center;
+}
+
+.card-body {
+    background: var(--color2);
+}
+
+.btn-primary {
+    background: var(--color7);
+}
+</style>
---
+++ mewwit/src/components/Posts.vue
@@ -0,0 +1,128 @@
+<script setup>
+import Newcomment from './NewComment.vue';
+import Comments from './Comments.vue';
+
+
+const props = defineProps({
+    post: {},
+    typeof: Object
+})
+
+</script>
+
+<template>
+    <div class="post d-flex">
+
+
+        <div class="card" style="max-width: 94vw;">
+            <div class="user__data">
+            <img v-if="!post.pic" class="img__user" src="https://picsum.photos/200">
+            <img v-else :src="post.pic" class="img__user" alt="...">
+            <h6>@{{ post.user }}</h6>
+        </div>
+            <div class="card-body">
+
+                <div class="card__fav d-flex">
+
+                    <h5 class="card-title">{{ post.title }}</h5>
+
+                    <p>{{ post.body }}</p>
+
+
+                </div>
+                <!--<RouterLink :to="'./details?_id='+post.id" class="btn btn-primary" :post="post" :key="post.id" >More info</RouterLink>
+                    -->
+            </div>
+            <img :src="post.img" class="card-img-start" alt="...">
+            <div class="buttons">
+                <button class="btn "><i class="bi bi-star"></i></button>
+                <!-- <i class="bi bi-star-fill"></i> -->
+                <button class="btn "><i class="bi bi-heart"></i></button>
+                <!-- <i class="bi bi-heart-fill"></i> -->
+                <button class="btn "><i class="bi bi-chat-dots"></i></button>
+            </div>
+
+        </div>
+
+        <div class="comments">
+            <Newcomment :postid="post.id"/>
+            <Comments :postid="post.id"/>
+
+
+        </div>
+
+    </div>
+
+</template>
+
+<style scoped>
+.user__data{
+    display: flex;
+    align-items: center;
+    justify-content: flex-start;
+}
+.comments{
+    background-image: linear-gradient(to bottom, #023047, #14425b, #255471, #356786, #457b9d);
+    background-size: cover;
+    background-repeat: no-repeat;
+    background-position: center;
+    border: 2px solid var(--color1);
+    border-radius: 0.5rem;
+}
+
+h3 {
+    text-align: end;
+    margin-right: 2rem;
+}
+
+.buttons {
+    text-align: center;
+}
+
+.bi {
+    font-size: 1.5rem;
+    color: var(--color4);
+}
+
+.post {
+   
+    margin: 1rem;
+    padding: 0.5rem;
+    flex-direction: column;
+    margin-top: 0;
+    border: 2px solid var(--color1);
+    border-radius: 0.5rem;
+}
+
+.img__user {
+    padding: 0.5rem;
+    width: 20%;
+    border-radius: 50%;
+    height: 25%;
+}
+
+.card-img-start {
+    width: 60%;
+    margin: 2rem;
+    border-radius: 5%;
+}
+
+.card__fav {
+
+    margin-left: 2rem;
+    padding: 0.5rem;
+    display: flex !important;
+    flex-direction: column;
+}
+
+.card {
+    color: var(--color2);
+    background: none !important;
+    ;
+    border: none;
+}
+
+.card-body {
+    display: flex;
+}
+</style>
